662442a0a6734bc8007734e53bd0f04c3ee6cd24
colindharrington <colindavidharrington@gmail.com>
2023-06-01 23:22:41 -0700

    progress

--- app/components/MainDisplay.tsx
+++ app/components/MainDisplay.tsx
@@ -1,39 +1,35 @@
-import {
-  Paper,
-  ToggleButton,
-  ToggleButtonGroup,
-  Typography,
-} from "@mui/material";
-import React, {useState, useEffect} from "react";
+import * as React from "react";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { Paper } from "@mui/material";
+import Random from "./Random";
+import Sorted from "./Sorted";
+import YourOwn from "./YourOwn";
 
-export default function MainDisplay() {
-  const [view, setView] = useState(0);
+export default function ToggleButtons() {
+  const [view, setView] = React.useState("random");
 
-  const handleAlignment = (
-    event: React.MouseEvent<HTMLElement>,
-    newView: string
-  ) => setView(newView);
+  const handleView = (event: any, newView: any) => {
+    setView(newView);
+  };
+
   return (
-    <Paper>
-      <ToggleButtonGroup
-        value={view}
-        exclusive
-        onChange={handleAlignment}
-    
-      >
-        <ToggleButton value={0} aria-label="left aligned">
-          <Typography>Random View</Typography>
-        </ToggleButton>
-        <ToggleButton value={1} aria-label="centered">
-          <Typography>Latest Collected</Typography>
-        </ToggleButton>
-        <ToggleButton value={2} aria-label="right aligned">
-          <Typography>View Other Address</Typography>
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </Paper>
+    <>
+      {/* <Paper elevation={3}>
+        <ToggleButtonGroup value={setView} exclusive onChange={handleView}>
+          <ToggleButton value="random">Random</ToggleButton>
+          <ToggleButton value="latest">Latest</ToggleButton>
+          <ToggleButton value="yours">Your own</ToggleButton>
+        </ToggleButtonGroup>
+      </Paper> */}
+      {/* {view === "random" && <Random />}
+      {view === "latest" && <Sorted />}
+      {view === "yours" && <YourOwn />} */}
+      <YourOwn />
+    </>
   );
 }
-
-
-function ToggleBar(
---
+++ app/components/YourOwn.tsx
@@ -0,0 +1,497 @@
+import React, { useEffect, useState } from "react";
+import { useQuery, gql, useLazyQuery } from "@apollo/client";
+import Image from "next/image";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  Divider,
+  Grid,
+  Paper,
+  Skeleton,
+  TextField,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const GET_DIGITAL_ART = gql`
+  query MyQuery($limit: Int!, $offset: Int!, $address: String!) {
+    token_holder(
+      where: { holder: { address: { _eq: $address } } }
+      offset: $offset
+      limit: $limit
+      order_by: { last_incremented_at: desc }
+    ) {
+      last_incremented_at
+      token {
+        artifact_uri
+        average
+        decimals
+        description
+        display_uri
+        extra
+        flag
+        highest_offer
+        is_boolean_amount
+        last_listed
+        last_metadata_update
+        level
+        lowest_ask
+        metadata
+        mime
+        name
+        ophash
+        rights
+        supply
+        symbol
+        thumbnail_uri
+        timestamp
+        tzip16_key
+        creators {
+          creator_address
+        }
+      }
+    }
+  }
+`;
+
+function ipfsHashToUrl(hash: string) {
+  if (!hash) return null;
+  const cleanHash = hash.replace("ipfs://", "");
+  return `https://ipfs.io/ipfs/${cleanHash}`;
+}
+
+type digitalArt = {
+  token: {
+    artifact_uri: string;
+    average: number;
+    decimals: number;
+    description: string;
+    display_uri: string;
+    extra: Array<{
+      mime_type: string;
+      size: number;
+      uri: string;
+    }>;
+    flag: string;
+    highest_offer: number;
+    is_boolean_amount: boolean;
+    last_listed: number;
+    last_metadata_update: number;
+    level: number;
+    lowest_ask: number;
+    metadata: string;
+    mime: string;
+    name: string;
+    ophash: string;
+    rights: string;
+    supply: number;
+    symbol: string;
+    thumbnail_uri: string;
+    timestamp: string;
+    tzip16_key: string;
+    creators: {
+      creator_address: string;
+    }[];
+  };
+};
+
+function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date
+    .getFullYear()
+    .toString()
+    .substring(2)} at ${formatHour(date.getHours())}:${formatMinute(
+    date.getMinutes()
+  )} ${formatAMPM(date.getHours())}`;
+  return formattedDate;
+}
+
+function formatHour(hour: number) {
+  if (hour === 0) {
+    return "12";
+  } else if (hour > 12) {
+    return (hour - 12).toString();
+  } else {
+    return hour.toString();
+  }
+}
+
+function formatMinute(minute: number) {
+  if (minute < 10) {
+    return `0${minute}`;
+  } else {
+    return minute.toString();
+  }
+}
+
+function formatAMPM(hour: number) {
+  if (hour < 12) {
+    return "AM";
+  } else {
+    return "PM";
+  }
+}
+export default function YourOwn() {
+  const router = useRouter();
+  const { tz } = router.query;
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const [digitalArt, setDigitalArt] = useState<digitalArt[]>([]);
+  const [page, setPage] = useState(1);
+  const [address, setAddress] = useState("");
+  const [searched, setSearched] = useState(false);
+
+  const handleChangeAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(event.target.value);
+  };
+
+  const itemsPerPage = 60;
+
+  const [getDigitalArt, { loading, error, data }] = useLazyQuery(
+    GET_DIGITAL_ART,
+    {
+      variables: {
+        limit: itemsPerPage,
+        offset: (page - 1) * itemsPerPage,
+        address: address,
+      },
+      onCompleted: (data) => {
+        setDigitalArt(data.token_holder);
+      },
+      onError: (error) => {
+        setDigitalArt([]);
+        console.log(error);
+      },
+    }
+  );
+
+  const handleButtonClick = () => {
+    getDigitalArt();
+    setSearched(true);
+    router.push({
+      pathname: router.pathname,
+      query: { tz: address },
+    });
+  };
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+
+  useEffect(() => {
+    if (tz) {
+      setAddress(tz as string);
+      getDigitalArt();
+      setSearched(true);
+    }
+  }, [tz]);
+
+  if (loading)
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "66vh",
+        }}
+      >
+        <CircularProgress
+          color={"error"}
+          thickness={0.5}
+          size={isMobile ? 50 : 200}
+        />
+      </Box>
+    );
+
+  return (
+    <div>
+      {!searched && (
+        <Paper
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            bgcolor: "transparent",
+            boxShadow: "none",
+            mt: 3,
+            mb: 2,
+            mx: "auto",
+            width: "100%",
+            height: "fit-content",
+            pt: "30vh",
+          }}
+        >
+          <TextField
+            label="Enter address here"
+            onChange={handleChangeAddress}
+            variant="outlined"
+            sx={{
+              "& .MuiInputBase-input": {
+                color: "#fff",
+              },
+              "& .MuiFormLabel-root": {
+                color: "#0f0",
+              },
+              "& .MuiOutlinedInput-root": {
+                "&.Mui-focused fieldset": {
+                  borderColor: "#0f0",
+                },
+              },
+              "& .MuiInputBase-input::placeholder": {
+                color: "#fff",
+              },
+              width: "100%",
+            }}
+          />
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+          >
+            <Button
+              variant="outlined"
+              onClick={handleButtonClick}
+              sx={{
+                mt: 2,
+                minWidth: "fit-content",
+              }}
+            >
+              Submit
+            </Button>
+          </motion.div>
+        </Paper>
+      )}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+      >
+        <Grid container spacing={1}>
+          {digitalArt &&
+            digitalArt.map((art, i) => (
+              <Grid item xs={12} md={4} lg={3} key={i}>
+                <Stack
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  {art.token.extra &&
+                  art.token.extra[0] &&
+                  art.token.extra[0].mime_type.startsWith("video/") ? (
+                    <div
+                      style={{
+                        backgroundImage: `url(${ipfsHashToUrl(
+                          art.token.display_uri || art.token.thumbnail_uri
+                        )})`,
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center center",
+                        width: "300px",
+                        height: "300px",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                      }}
+                    />
+                  ) : art.token.extra &&
+                    art.token.extra[0] &&
+                    (art.token.extra[0].mime_type === "image/jpeg" ||
+                      art.token.extra[0].mime_type === "image/png" ||
+                      art.token.extra[0].mime_type === "image/gif") ? (
+                    <div>
+                      <ImageItem art={art} />
+                    </div>
+                  ) : art.token.extra[0] &&
+                    art.token.extra[0].mime_type === "audio/mpeg" ? (
+                    <audio
+                      src={ipfsHashToUrl(art.token.extra[0].uri) as string}
+                      controls
+                    />
+                  ) : art.token.extra[0] &&
+                    art.token.extra[0].mime_type ===
+                      "application/x-directory" ? (
+                    <IframeItem art={art} />
+                  ) : art.token.display_uri && art.token.display_uri ? (
+                    <div
+                      style={{
+                        backgroundImage: `url(${ipfsHashToUrl(
+                          art.token.display_uri
+                        )})`,
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center center",
+                        width: "300px",
+                        height: "300px",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                      }}
+                    />
+                  ) : (
+                    <Typography>Unsupported file format</Typography>
+                  )}
+                  {art.token.name && (
+                    <Stack sx={{ mt: 1 }}>
+                      <Typography
+                        variant="caption"
+                        align={"center"}
+                        color="#999"
+                      >
+                        {art.token.name}
+                      </Typography>
+                    </Stack>
+                  )}
+                </Stack>
+              </Grid>
+            ))}
+        </Grid>
+        {searched && (
+          <Stack spacing={2}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "#444",
+                my: 2,
+              }}
+            >
+              <Box style={{ display: "flex", justifyContent: "center" }}>
+                <Pagination
+                  count={10}
+                  page={page}
+                  onChange={handlePageChange}
+                />
+              </Box>
+            </Paper>
+          </Stack>
+        )}
+      </motion.div>
+    </div>
+  );
+}
+
+function ImageItem({ art: art }: { art: digitalArt }) {
+  const [itemOpen, setItemOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  return (
+    <div>
+      {loading ? (
+        <Skeleton variant="rectangular" width={300} height={300} />
+      ) : (
+        <div
+          onClick={() => setItemOpen(true)}
+          style={{
+            backgroundImage: `url(${ipfsHashToUrl(art.token.extra[0].uri)})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            width: "300px",
+            height: "300px",
+            maxWidth: "100%",
+            maxHeight: "100%",
+          }}
+        />
+      )}
+      <Dialog
+        open={itemOpen}
+        onClose={() => setItemOpen(false)}
+        sx={{
+          "& .MuiBackdrop-root": {
+            backgroundColor: "rgba(0, 0, 0, 0.95)",
+          },
+          "& .MuiDialog-paper": {
+            backgroundColor: "black",
+          },
+        }}
+      >
+        <div
+          onClick={() => setItemOpen(false)}
+          style={{
+            backgroundImage: `url(${ipfsHashToUrl(art.token.extra[0].uri)})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            width: "500px",
+            height: "500px",
+            maxWidth: "100%",
+            maxHeight: "100%",
+            backgroundColor: "black",
+          }}
+        />
+        <Stack
+          sx={{
+            bgcolor: "black",
+            color: "white",
+            p: 1,
+            display: "flex",
+          }}
+        >
+          <Typography variant="h4">Title: {art.token.name}</Typography>
+          <Typography>Minted: {formatDate(art.token.timestamp)}</Typography>
+          <Typography>Total Supply: {art.token.supply}</Typography>
+          <Divider sx={{ bgcolor: "white", my: 1 }} />
+          <Link
+            href={`https://objkt.com/profile/${art.token.creators[0].creator_address}/created`}
+            target="_blank"
+            rel="noopener"
+          >
+            <Typography variant="caption">Link to creator on OBJKT</Typography>
+          </Link>
+          <Typography variant="caption">
+            Description: {art.token.description}
+          </Typography>
+          <Box sx={{ mt: 2 }}>
+            <Button
+              variant="outlined"
+              fullWidth
+              onClick={() => setItemOpen(false)}
+            >
+              Close
+            </Button>
+          </Box>
+        </Stack>
+      </Dialog>
+    </div>
+  );
+}
+
+function IframeItem({ art: art }: { art: digitalArt }) {
+  const [itemOpen, setItemOpen] = useState(false);
+  return (
+    <div>
+      <div
+        onClick={() => setItemOpen(true)}
+        style={{
+          backgroundImage: `url(${ipfsHashToUrl(art.token.display_uri)})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          width: "300px",
+          height: "300px",
+          maxWidth: "100%",
+          maxHeight: "100%",
+        }}
+      />
+      <Dialog open={itemOpen} onClose={() => setItemOpen(false)}>
+        <iframe
+          src={ipfsHashToUrl(art.token.extra[0].uri) as string}
+          width="600"
+          height="600"
+          style={{ border: 0 }}
+        />
+      </Dialog>
+    </div>
+  );
+}
